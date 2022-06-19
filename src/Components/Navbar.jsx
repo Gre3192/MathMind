15347b10f82b5ff6ecfc363140f5b41a2c3bc3d8
gre3192 <g.distefano@contrader.it>
2022-06-19 02:05:24 +0200

    Addes Tailwind + Headless

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  return <>
+  
+  <div className="bg-zinc-500"> ciao </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  </>;
+};
+
+export default Navbar;
